2efefc8902e5b875a25a95e134e9d3cabb7e6edc
scrive66 <adobe.shaflon@gmail.com>
2020-08-02 08:33:14 +0900

    ディレクトリ移動

--- WindowsFormsApp1/01_Form/Frm_Main.Designer.cs
+++ WindowsFormsApp1/01_Form/Frm_Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class Frm_Main
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Frm_Main";
+            this.Text = "メインウインドウ";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ WindowsFormsApp1/01_Form/Frm_Main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
